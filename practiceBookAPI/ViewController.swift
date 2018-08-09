8f20ea3b555505566f8b754c56bb477c042c174a
Yuhi Tokoro <tokoroyuhi@gmail.com>
2018-08-09 09:07:27 +0900

    テスト

--- practiceBookAPI/ViewController.swift
+++ practiceBookAPI/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("ss")
+        print("aaaa")
     }
 
     override func didReceiveMemoryWarning() {
